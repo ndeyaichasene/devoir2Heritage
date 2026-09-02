5f419e5ebbabe40a1ce9ace2640dcea04bb9db68
Ndeye Aissatou Sene <ndeyaichasene@gmail.com>
2026-09-02 04:12:35 +0000

    feat: ajouter le schéma de la base

---
+++ database/schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE copies(
+    id SERIAL PRIMARY KEY,
+    dateDepot TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    noteBrute NUMERIC(4,2) NOT NULL,
+    noteFinale NUMERIC(4,2) NOT NULL,
+    penaliteAppliquee BOOLEAN NOT NULL,
+    dateLimite TIMESTAMP NOT NULL
+);
+
+INSERT INTO copies (
+    dateDepot,
+    noteBrute,
+    noteFinale,
+    penaliteAppliquee,
+    dateLimite
+)
+VALUES (
+    CURRENT_TIMESTAMP,
+    16.50,
+    14.50,
+    TRUE,
+    '2026-09-15 23:59:59'
+);
+
+INSERT INTO copies (
+    noteBrute,
+    noteFinale,
+    penaliteAppliquee,
+    dateLimite
+)
+VALUES (
+    18.00,
+    18.00,
+    FALSE,
+    '2026-09-15 23:59:59'
+);
+
+SELECT * FROM copies;

9e2f16574f5275ad2f312f08496cb666fa11da8e
Ndeye Aissatou Sene <ndeyaichasene@gmail.com>
2026-09-03 00:14:14 +0000

    feat: appliquer une pénalité de deux points

--- src/Service/CalculNoteAvecRetardService.php
+++ src/Service/CalculNoteAvecRetardService.php
@@ -9,7 +9,7 @@ class CalculNoteAvecRetardService implements CalculNoteInterface{
     public function calculerNote(float $noteBrute, \DateTime $dateDepot, \DateTime $dateLimite): float
     {
         if ($dateDepot > $dateLimite) {
-           $noteFinale = max(0,$noteBrute - 2);
+           $noteFinale = $noteBrute - 2;
         }else {
             $noteFinale = $noteBrute;
         }
